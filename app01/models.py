5fbdf533274d74b58624001cce86e72a35901cd9
linbohao <2804809928@qq.com>
2020-03-21 09:38:35 +0800

    'register'

--- app01/models.py
+++ app01/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class UserInfo(models.Model):
+    username = models.CharField(max_length=32, verbose_name='用户名')
+    password = models.CharField(max_length=32, verbose_name='密码')
+    email = models.EmailField(max_length=32, verbose_name='邮箱')
+    phone = models.CharField(max_length=11, verbose_name='手机号')
--- app01/views.py
+++ app01/views.py
@@ -1,3 +1,4 @@
+from django.core.validators import RegexValidator
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.conf import settings
@@ -5,6 +6,28 @@ from django.conf import settings
 # Create your views here.
 import random
 from utils.tencent.sms import send_sms_single
+from django import forms
+from app01 import models
+
+
+class RegisterModelForm(forms.ModelForm):
+    phone = forms.CharField(label='手机号', validators=[RegexValidator
+                                                     (r'^(1[3\4\5\6\7\8\9])\d{9}$', '手机号格式错误'), ])
+    password = forms.CharField(label='密  码', widget=forms.PasswordInput
+    (attrs={'class': 'form-control', 'placeholder': '输入密码'}))
+    confirm_password = forms.CharField(label='重复密码', widget=forms.PasswordInput
+    (attrs={'class': 'form-control', 'placeholder': '重复密码'}))
+    code = forms.CharField(label='验证码', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': '输入验证码'}))
+
+    class Meta:
+        model = models.UserInfo
+        fields = ['username', 'email', 'password', 'confirm_password', 'phone', 'code']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.widget.attrs['placeholder'] = '请输入%s' % (field.label,)
 
 
 def send_sms(request):
@@ -19,3 +42,8 @@ def send_sms(request):
         return HttpResponse('发送成功')
     else:
         return HttpResponse('errmsg')
+
+
+def register(request):
+    form = RegisterModelForm()
+    return render(request, 'register.html', {'form': form})
